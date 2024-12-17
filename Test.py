69a24d2a76b33be306dc549cc01a70f4805bbde9
gattami <m.gattami@gmail.com>
2024-12-17 11:11:13 +0100

    dagens klass

--- Test.py
+++ Test.py
@@ -1,1 +1,20 @@
-print("hello world")
+
+'''
+x = 10
+y = 20
+print(f"summan av {x} + {y} är", (x+y))      
+
+
+
+namn = "Mila"
+ålder = 38
+
+print(f"Hej, {namn}! Du är {ålder} år gammal.")
+'''
+
+
+x="123"
+y=2
+z= int(x)
+a=z*y
+print(a)
