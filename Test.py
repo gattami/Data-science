cdaf0d5c78aec6cc23ff35c8ed4ad1427498cd7a
gattami <m.gattami@gmail.com>
2024-12-20 11:16:35 +0100

    dagens lektion

--- Test.py
+++ Test.py
@@ -1,4 +1,4 @@
-
+'''''
 text = "   Python är roligt!   "
 rensad_text = text.strip()
 print(rensad_text)
@@ -14,8 +14,6 @@ namn = "Mila jackson"
 print (f"hej mitt namn är {namn} jag är {ålder} år GAMMAL.")
 
 
-skapa en dictionary för en produkt
-produkt ={"namn: " Laptoop", "pris": 10000, "lager":50}
 
 produkt = {"namn": "Laptop", "pris": 10000, "lager": 50}
 print(f"Produktens namn: {produkt['namn']}")
@@ -24,3 +22,36 @@ print(f"Ny kategori tillagd: {produkt['kategori']}")
 produkt["lager"] = 40
 print(f"Uppdaterat lager: {produkt['lager']}")
 print("Uppdaterad produktinformation:", produkt)
+
+
+age = int(input("Ange din ålder: " ))
+if age > 18:
+ print("GÅ UT, DU FÅR INTE VARA HÄR")
+
+
+telefonnummer = input("Ange ditt telefonnummer: ")
+
+
+telefonnummer = input("Ange ditt telefonnummer: ")
+
+if len(telefonnummer) >= 10 and telefonnummer.isdigit():
+    print(f"Ditt telefonnummer är: {telefonnummer}")
+else:
+    print("Telefonnumret måste vara minst 10 siffror och endast innehålla siffror!")
+
+
+telefonnummer = input("Ange ditt telefonnummer: ")
+if telefonnummer.isdigit() and len(telefonnummer) >= 10:
+    print(f"Ditt telefonnummer är: {telefonnummer}")
+else:
+    print("Telefonnumret måste vara minst 10 siffror och endast innehålla siffror!")
+print("Telefonnumret måste vara minst 10 siffror och endast innehålla siffror!")
+'''
+
+password = ""
+while password != "hemligt":  
+    password = input("Ange lösenord: ")
+    print("Rätt lösenord!") 
+    if password != "hemligt": 
+        print("Fel lösenord, försök igen!")
+        
