59cfa54d009aac2dbdf56c600720b5d172020ed4
gattami <m.gattami@gmail.com>
2024-12-20 09:24:16 +0100

    dagenslektion

--- Test.py
+++ Test.py
@@ -12,3 +12,15 @@ print(text.replace("bäst", "sådär"))
 namn = "Mila jackson"
 ålder = "39"
 print (f"hej mitt namn är {namn} jag är {ålder} år GAMMAL.")
+
+
+skapa en dictionary för en produkt
+produkt ={"namn: " Laptoop", "pris": 10000, "lager":50}
+
+produkt = {"namn": "Laptop", "pris": 10000, "lager": 50}
+print(f"Produktens namn: {produkt['namn']}")
+produkt["kategori"] = "Elektronik"
+print(f"Ny kategori tillagd: {produkt['kategori']}")
+produkt["lager"] = 40
+print(f"Uppdaterat lager: {produkt['lager']}")
+print("Uppdaterad produktinformation:", produkt)
