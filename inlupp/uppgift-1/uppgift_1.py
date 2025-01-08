c5cda05b7ac35fdc71ff02a4f94235c50af176d5
gattami <m.gattami@gmail.com>
2025-01-08 14:06:15 +0100

    uppgift 2

--- inlupp/uppgift-1/uppgift_1.py
+++ inlupp/uppgift-1/uppgift_1.py
@@ -1,3 +1,4 @@
+'''
 # Uppgift 1
 # Skapa en funktion is_odd(x) som returnerar True om x är udda och False om x är jämnt.
 # def funktions_namn(variabel_namn: datatyp) -> returtyp:
@@ -11,4 +12,9 @@ def is_odd(x: int) -> bool:
     return x % 2 != 0
 
 print (is_odd (10))
+'''
 
+
+my_list = [20, 30, 40, 50]
+result = sum_list(my_list)
+print(result) 
--- inlupp/uppgift-2/uppgift_2.py
+++ inlupp/uppgift-2/uppgift_2.py
@@ -1,9 +1,14 @@
-# Uppgift 2
-# Skapa en funktion sum_list(numbers) som returnerar summan av alla siffror i listan.
 
-def funktions_namn(variabel_namn: datatyp) -> returtyp:
-    """
-    Skriv beskrivning här.
-    """
-    pass # Ta bort denna rad och skriv din kod här
-dfgdfff
+ 
+from typing import List
+
+def sum_list(numbers: List[int]) -> int:
+    total = 0  
+    for number in numbers:  
+        total += number  
+    return total 
+
+
+numbers = [10, 20, 30, 40, 50]  
+result = sum_list(numbers)  
+print(result)  
