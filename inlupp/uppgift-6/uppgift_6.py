6c60bd6b2ea95a3454d382ba075fc59746f1bed6
gattami <m.gattami@gmail.com>
2025-02-28 11:38:38 +0100

    uppgift6

--- inlupp/uppgift-6/uppgift_6.py
+++ inlupp/uppgift-6/uppgift_6.py
@@ -1,8 +1,9 @@
 # Uppgift 6
-# Skapa en funktion multiplication_table(n, limit) som returnerar multiplikationstabellen för n upp till limit i en lista.
+# Skapa en funktion multiplication_table(n, limit) som returnerar
+# multiplikationstabellen för n upp till limit i en lista.
 
-def funktions_namn(variabel_namn: datatyp) -> returtyp:
+def multiplication_table(n: int, limit: int) -> list:
     """
     Skriv beskrivning här.
     """
-    pass # Ta bort denna rad och skriv din kod här
+    return [n * i for i in range(1, limit + 1)]
